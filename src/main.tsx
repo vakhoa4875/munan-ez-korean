e4c9e65965f56501619e9e92d8d48e4232aac4d5
kdev <vakhoa4875+ithd@gmail.com>
2025-03-05 13:57:39 +0700

    init: configure primereact + react toastify

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,27 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { PrimeReactProvider } from 'primereact/api';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { ToastContainer } from 'react-toastify';
+import App from './App.tsx';
+import './index.css';
+
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <PrimeReactProvider>
+    <StrictMode>
+      <ToastContainer
+        position="top-right"
+        autoClose={3500}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
+      <App />
+    </StrictMode>
+  </PrimeReactProvider>,
 )
