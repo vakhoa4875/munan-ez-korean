8ba8e0110a25299b5d1ebfb695bad1c54c42ecdb
Vu Anh Khoa <128579628+vakhoa4875@users.noreply.github.com>
2025-03-11 15:47:54 +0700

    feat: footer UI block

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { Outlet } from "react-router-dom";
+import Footer from "./Footer";
 import { TopNavbar } from "./Navbar";
 import { RightSideBar } from "./Sidebar";
 
@@ -13,6 +14,7 @@ export default function Layout() {
       <div className="p-4 w-full">
         <Outlet />
       </div>
+      <Footer />
     </div>
   );
 }
