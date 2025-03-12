a5c2be26acd567a0614f25264135c85cfa26b5f4
Vu Anh Khoa <128579628+vakhoa4875@users.noreply.github.com>
2025-03-12 15:45:09 +0700

    temp: save Homepage state

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,7 +2,13 @@ import AdvisingForm from "../components/AdvisingForm";
 
 export default function Home() {
     return (
-      <div className="flex justify-center items-center bg-white">
+      <div className="flex justify-center items-center bg-white flex-col">
+        <img src="/1.webp" alt="" />
+        <img src="/2.gif" alt="" />
+        <img src="/3.gif" alt="" />
+        <img src="/4.webp" alt="" />
+        <img src="/5.webp" alt="" />
+        <img src="/6.webp" alt="" />
         <AdvisingForm />
       </div>
     )
